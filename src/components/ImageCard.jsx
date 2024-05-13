1b4a14e1b78e5950a06a4c8482d8a24a6cca58fd
Shubham <shubhamsarkar9654@gmail.com>
2024-05-13 17:47:44 +0530

    f ied csss

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -10,16 +10,33 @@ const ImageCard = ({ image }) => {
     setSpans(calculatedSpans);
   };
 
+  const handleDownload = async () => {
+    try {
+      const response = await fetch(urls.regular);
+      const blob = await response.blob();
+      const url = window.URL.createObjectURL(new Blob([blob]));
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', 'image.jpg');
+      document.body.appendChild(link);
+      link.click();
+      link.parentNode.removeChild(link);
+    } catch (error) {
+      console.error('Error downloading image:', error);
+    }
+  };
+
   const { description, urls } = image;
 
   return (
-    <div style={{ gridRowEnd: `span ${spans}` }} className='relative'>
+    <div style={{ gridRowStart: `span ${spans}` }} className='relative'>
       <img
         onLoad={onImgLoad}
         ref={imageRef}
         alt={description}
         src={urls.regular}
       />
+      <img src="/download.svg" className='!w-8 h-8 absolute bottom-[20px] left-0 cursor-pointer' onClick={handleDownload} />
     </div>
   );
 };
