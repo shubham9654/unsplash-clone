db6116d06b017326b2d4628cca94ac94e88450c1
Shubham <shubhamsarkar9654@gmail.com>
2024-05-13 15:52:22 +0530

    added debounce

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,15 @@
+import { useEffect, useState } from "react";
+import { useDebounce } from "use-debounce";
+
 const SearchBar = ({ whenSubmit }) => {
+  const [text, setText] = useState();
+  const [value] = useDebounce(text, 1000);
+
+
+  useEffect(() => {
+    whenSubmit(value)
+  }, [value])
+  
   return (
     <div className="ui segment">
       <form className="ui form">
@@ -8,7 +19,7 @@ const SearchBar = ({ whenSubmit }) => {
             type="text"
             onChange={(e) => {
               e.preventDefault()
-              whenSubmit(e.target.value)
+              setText(e.target.value)
             }}
           />
         </div>
